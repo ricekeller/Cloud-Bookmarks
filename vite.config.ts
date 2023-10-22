b9761f0385d97aaae74717300de4e28cc60c4c25
Yan Mi <ricekeller@gmail.cn>
2023-10-21 20:50:07 -0400

    copy manifest.json to dist folder when building

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { copy } from 'vite-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [
+		vue(),
+		copy(
+			[
+				{
+					src: './manifest.json',
+					dest: 'dist/',
+				},
+			],
+			{
+				hook: 'writeBundle',
+			}
+		),
+	],
+});
